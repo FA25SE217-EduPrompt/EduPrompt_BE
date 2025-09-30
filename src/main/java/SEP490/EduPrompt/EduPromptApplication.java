a4e39e9b33a47effb2dede9a92e513b781c05ead
somethinn <irtsomethinn@gmail.com>
2025-09-30 21:06:05 +0700

    add database schema v1

--- src/main/java/SEP490/EduPrompt/EduPromptApplication.java
+++ src/main/java/SEP490/EduPrompt/EduPromptApplication.java
@@ -2,8 +2,12 @@ package SEP490.EduPrompt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "SEP490.EduPrompt.repo")
+@EntityScan(basePackages = "SEP490.EduPrompt.model")
 public class EduPromptApplication {
 
 	public static void main(String[] args) {
