5d8f00af42d9c40a3a587a0010421e86971d5253
somethinn <irtsomethinn@gmail.com>
2025-09-30 21:07:54 +0700

    config for swagger and test run

---
+++ src/main/java/SEP490/EduPrompt/controller/TestController.java
@@ -0,0 +1,14 @@
+package SEP490.EduPrompt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Lord Tri Nguyen";
+    }
+}
