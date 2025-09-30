272fa0f8c53a79dde1f1cc99781af036a2e2efc1
somethinn <irtsomethinn@gmail.com>
2025-09-30 16:40:21 +0700

    init project

---
+++ src/main/java/SEP490/EduPrompt/EduPromptApplication.java
@@ -0,0 +1,13 @@
+package SEP490.EduPrompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SEP490/EduPrompt/EduPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package SEP490.EduPrompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
