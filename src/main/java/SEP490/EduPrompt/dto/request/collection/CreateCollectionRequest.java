df39c1bb36d0ff9dbca1d090fb937c020fb96867
somethinn <irtsomethinn@gmail.com>
2025-10-09 11:37:13 +0700

    create response/request dtos for collection service

---
+++ src/main/java/SEP490/EduPrompt/dto/request/collection/CreateCollectionRequest.java
@@ -0,0 +1,21 @@
+package SEP490.EduPrompt.dto.request.collection;
+
+import SEP490.EduPrompt.model.Tag;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CreateCollectionRequest(
+        @NotBlank(message = "Name is required") @Size(max = 255) String name,
+        String description,
+
+        @NotNull @Pattern(regexp = "(private|public|school|group)", message = "Invalid visibility") String visibility,
+        List<Tag> tags,
+
+        UUID groupId
+) {
+}
