3809ce215d25c9b7408213af0e56cbed726ab298
somethinn <irtsomethinn@gmail.com>
2025-10-09 11:42:44 +0700

    use custom UserPrincipal to store user auth data

---
+++ src/main/java/SEP490/EduPrompt/service/auth/UserPrincipal.java
@@ -0,0 +1,71 @@
+package SEP490.EduPrompt.service.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserPrincipal implements UserDetails {
+
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    private UUID userId;
+    private String userName;
+    private String email;
+    private String role;
+    private UUID schoolId;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    //default userDetail fields, dont touch it
+    private boolean accountExpired;
+    private boolean accountLocked;
+    private boolean credentialsExpired;
+    private boolean disabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(ROLE_PREFIX + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
