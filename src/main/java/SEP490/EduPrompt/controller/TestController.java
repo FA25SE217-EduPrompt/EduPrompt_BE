ca95ccfa2c12081c2a16b9c1861385ef312d5e15
somethinn <irtsomethinn@gmail.com>
2025-10-10 23:36:28 +0700

    add test endpoint for redis

--- src/main/java/SEP490/EduPrompt/controller/TestController.java
+++ src/main/java/SEP490/EduPrompt/controller/TestController.java
@@ -1,14 +1,31 @@
 package SEP490.EduPrompt.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @GetMapping("/redis")
+    public String testRedis() {
+        redisTemplate.opsForValue().set("test-key", "test-value", Duration.ofMinutes(1));
+        String value = redisTemplate.opsForValue().get("test-key");
+        return "Redis finally working! Fuck it we ball! : " + value;
+    }
+
     @GetMapping
     public String test() {
         return "Lord Tri Nguyen";
     }
+
+
 }
