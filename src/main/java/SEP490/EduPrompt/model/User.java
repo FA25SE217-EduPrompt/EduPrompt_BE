369fab62ae49ece679d0255db95d1d0c054feef0
somethinn <irtsomethinn@gmail.com>
2025-10-12 19:52:09 +0700

    enhance email uniqueness in user & user_auth

--- src/main/java/SEP490/EduPrompt/model/User.java
+++ src/main/java/SEP490/EduPrompt/model/User.java
@@ -39,7 +39,7 @@ public class User {
     @Column(name = "phone_number", length = 50)
     private String phoneNumber;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "role", nullable = false, length = 100)
