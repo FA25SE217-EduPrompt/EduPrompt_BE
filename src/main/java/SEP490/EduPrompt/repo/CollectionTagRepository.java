e376c7f8ae0616d20b3ef896f971b3f638d03bb0
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2025-10-19 23:27:17 +0700

    fix repos related to collection service

--- src/main/java/SEP490/EduPrompt/repo/CollectionTagRepository.java
+++ src/main/java/SEP490/EduPrompt/repo/CollectionTagRepository.java
@@ -3,6 +3,14 @@ package SEP490.EduPrompt.repo;
 import SEP490.EduPrompt.model.CollectionTag;
 import SEP490.EduPrompt.model.CollectionTagId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface CollectionTagRepository extends JpaRepository<CollectionTag, CollectionTagId> {
+    @Query("SELECT ct FROM CollectionTag ct JOIN FETCH ct.tag WHERE ct.id.collectionId = :collectionId")
+    List<CollectionTag> findByCollectionId(UUID collectionId);
+
+    void deleteByCollectionId(UUID collectionId);
 }
