c56c0df3e716297ac2151b8984df3fc338aada9a
somethinn <irtsomethinn@gmail.com>
2025-10-24 17:34:11 +0700

    constraint for AddTagRequest

--- src/main/java/SEP490/EduPrompt/dto/request/AddTagRequest.java
+++ src/main/java/SEP490/EduPrompt/dto/request/AddTagRequest.java
@@ -1,6 +1,7 @@
 package SEP490.EduPrompt.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 /***
  * Add tag to collection/prompt if already existed, if none, create a new one and add
@@ -10,7 +11,10 @@ import jakarta.validation.constraints.NotBlank;
 
 public record AddTagRequest(
         @NotBlank
+        @Size(max = 50)
         String type,
+
+        @Size(max = 255)
         @NotBlank
         String value
 ) {
