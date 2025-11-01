ee37e6ae6efb990626d1ca244b801dd51db2634b
somethinn <irtsomethinn@gmail.com>
2025-11-01 21:41:49 +0700

    new constructor for AiProviderException exception and new ExceptionCode

--- src/main/java/SEP490/EduPrompt/exception/generic/ExceptionCode.java
+++ src/main/java/SEP490/EduPrompt/exception/generic/ExceptionCode.java
@@ -3,5 +3,6 @@ package SEP490.EduPrompt.exception.generic;
 public enum ExceptionCode {
     INVALID_ACTION,
     QUOTA_EXCEEDED,
-    FAILED_SIGNATURE_GENERATION
+    FAILED_SIGNATURE_GENERATION,
+    AI_PROVIDER_ERROR
 }
