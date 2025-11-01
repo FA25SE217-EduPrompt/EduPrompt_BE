8ddbfadda54da3fcd328817cda6df6e51eb22f7c
somethinn <irtsomethinn@gmail.com>
2025-11-01 21:38:55 +0700

    update prompt test dtos

--- src/main/java/SEP490/EduPrompt/dto/request/prompt/PromptTestRequest.java
+++ src/main/java/SEP490/EduPrompt/dto/request/prompt/PromptTestRequest.java
@@ -1,9 +1,11 @@
 package SEP490.EduPrompt.dto.request.prompt;
 
 import SEP490.EduPrompt.enums.AiModel;
+import lombok.Builder;
 
 import java.util.UUID;
 
+@Builder
 public record PromptTestRequest(
         UUID promptId,
         AiModel aiModel,
