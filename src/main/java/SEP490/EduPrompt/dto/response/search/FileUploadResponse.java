3912ce5f3eb80fb6adb8ec467644803c14b031e1
somethinn <irtsomethinn@gmail.com>
2025-11-18 00:19:04 +0700

    adjust dtos based on refactored services

--- src/main/java/SEP490/EduPrompt/dto/response/search/FileUploadResponse.java
+++ src/main/java/SEP490/EduPrompt/dto/response/search/FileUploadResponse.java
@@ -7,6 +7,8 @@ import java.util.UUID;
 @Builder
 public record FileUploadResponse(
         String fileId,
+        String documentId,
+        String operationId,
         String status, // PROCESSING, ACTIVE, FAILED
         UUID promptId
 ) {
