27bdc8dd5fc50f93e5dc903de2ad78b1ae375d8b
somethinn <irtsomethinn@gmail.com>
2025-11-18 00:21:15 +0700

    config for file search store

--- src/main/java/SEP490/EduPrompt/controller/TestController.java
+++ src/main/java/SEP490/EduPrompt/controller/TestController.java
@@ -1,11 +1,11 @@
 package SEP490.EduPrompt.controller;
 
-import SEP490.EduPrompt.service.search.GeminiClientService;
-import com.google.genai.types.File;
-import com.google.genai.types.FileSearchStore;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
@@ -18,7 +18,7 @@ public class TestController {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final SpringTemplateEngine templateEngine;
-    private final GeminiClientService service;
+//    private final GeminiClientService service;
 
     @GetMapping("/redis")
     public String testRedis() {
@@ -45,19 +45,21 @@ public class TestController {
         return templateEngine.process("account-verification-email", context);
     }
 
-    @GetMapping("/get-file")
-    public String testGetFile() {
-        File file = service.getFile("files/oz306g4ocoj7");
-        return file.toJson();
-    }
-
-    @PostMapping("/store")
-    public String createStore() {
-        FileSearchStore store = service.createPromptStore("eduprompt");
-        return store.toJson();
-    }
-    @PostMapping("/import-file")
-    public String importFileToStore() {
-        return service.importFileToStore("files/oz306g4ocoj7");
-    }
+//    @GetMapping("/get-file")
+//    public String testGetFile(@RequestParam String fileName) {
+//        String name = fileName == null ? "files/r2zn0sr9aeg3" : fileName;
+//        File file = service.getFile(name);
+//        return file.toJson();
+//    }
+//
+//    @PostMapping("/store")
+//    public String createStore() {
+//        FileSearchStore store = service.createPromptStore("eduprompt");
+//        return store.toJson();
+//    }
+//    @PostMapping("/import-file")
+//    public String importFileToStore(@RequestParam String fileName) {
+//        String name = fileName == null ? "files/r2zn0sr9aeg3" : fileName;
+//        return service.importFileToStore(name);
+//    }
 }
