457c503d3cb38407e693aaffc1fcb6f22aa04da2
somethinn <irtsomethinn@gmail.com>
2025-12-02 22:00:48 +0700

    chore: add new properties in user quota response

--- src/main/java/SEP490/EduPrompt/dto/response/quota/UserQuotaResponse.java
+++ src/main/java/SEP490/EduPrompt/dto/response/quota/UserQuotaResponse.java
@@ -8,11 +8,18 @@ import java.util.UUID;
 @Builder
 public record UserQuotaResponse(
         UUID userId,
-        int testingQuotaRemaining,
-        int testingQuotaLimit,
-        int optimizationQuotaRemaining,
-        int optimizationQuotaLimit,
-        Instant quotaResetDate,
-        Integer individualTokenLimit, Integer individualTokenRemaining
+        Integer testingQuotaLimit,
+        Integer testingQuotaRemaining,
+        Integer optimizationQuotaLimit,
+        Integer optimizationQuotaRemaining,
+        Integer individualTokenLimit,
+        Integer individualTokenRemaining,
+        Integer promptActionLimit,
+        Integer promptActionRemaining,
+        Integer collectionActionLimit,
+        Integer collectionActionRemaining,
+        Integer promptUnlockLimit,
+        Integer promptUnlockRemaining,
+        Instant quotaResetDate
 ) {
 }
