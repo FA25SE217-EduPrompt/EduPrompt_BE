f901d651bc31c1d89dcc40da397fec6ba8d0303d
somethinn <irtsomethinn@gmail.com>
2025-12-10 22:11:44 +0700

    chore: drop column subscription_id in users table; update endpoint prefix

--- src/main/java/SEP490/EduPrompt/controller/SchoolController.java
+++ src/main/java/SEP490/EduPrompt/controller/SchoolController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/school")
+@RequestMapping("api/school")
 @RequiredArgsConstructor
 public class SchoolController {
     private final SchoolService schoolService;
