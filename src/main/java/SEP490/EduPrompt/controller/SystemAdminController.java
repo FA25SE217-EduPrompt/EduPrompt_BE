28e824595886d6381864828bb7461d5abd520ab2
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2025-12-15 19:33:22 +0700

    minor changes

--- src/main/java/SEP490/EduPrompt/controller/SystemAdminController.java
+++ src/main/java/SEP490/EduPrompt/controller/SystemAdminController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 public class SystemAdminController {
 
