edb302d56d7a7525c9f1294cbc85fb9255c3c573
somethinn <irtsomethinn@gmail.com>
2026-01-06 23:24:01 +0700

    feat(repo): add repos for lesson, subject, and prompt version

--- src/main/java/SEP490/EduPrompt/repo/SubjectRepository.java
+++ src/main/java/SEP490/EduPrompt/repo/SubjectRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, UUID> {
     List<Subject> findByNameContaining(String name);
 
-    Subject findByNameIgnoreCase(String name);
+    Optional<Subject> findByNameIgnoreCase(String name);
+
 }
