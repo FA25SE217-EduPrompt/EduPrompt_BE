c032cec2a3609193ec6a255c9579c8ffff8dfc1f
somethinn <irtsomethinn@gmail.com>
2026-01-08 20:30:51 +0700

    refactor: remove redundant annotation

--- src/main/java/SEP490/EduPrompt/EduPromptApplication.java
+++ src/main/java/SEP490/EduPrompt/EduPromptApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,7 +12,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EntityScan(basePackages = "SEP490.EduPrompt.model")
 @EnableCaching
 @EnableScheduling
-@EnableAsync
 public class EduPromptApplication {
 
     public static void main(String[] args) {
